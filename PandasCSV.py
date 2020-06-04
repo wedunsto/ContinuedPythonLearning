86bd950b7ee44403ccf1f201404a1273f8d23eb6
William Dunston <wdunston@Lenovo.com>
2020-06-04 11:52:33 -0400

    Added Pandas CSV file functions

---
+++ PandasCSV.py
@@ -0,0 +1,19 @@
+#Objective: Introduce working with Pands and csv files
+#Opening and saving csv files
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("Sports.csv",index_col="Brand") #Saves the csv file as a DataFrame
+#Sets the index to the brands
+print(df,"\n\n")
+
+BoardsSeries = pd.read_csv("Sports.csv",usecols=["Brand"], squeeze = True) #Save only the column Brand from the CSV as a Series
+print(BoardsSeries,"\n\n")
+
+Newdf = pd.read_csv("Sports.csv",index_col="Brand",skiprows=[1]) #Saves the csv file as a Dataframe, skipping the second row
+print(Newdf,"\n\n")
+
+EditedDF = df["Event"] = ["Super pipe", "Half pipe"] #Create a new DataFrame with a new column
+print(EditedDF,"\n\n")
+EditedDF.to_csv('./EditedSports.csv') #Save the edited DataFrame to a new .CSV file
