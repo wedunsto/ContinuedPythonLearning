14ba8fd3e8997b42be0fa95460be281da7469754
wedunsto <wedunsto@aggies.ncat.edu>
2020-06-06 16:30:47 -0400

    Fixed Pandas issues and added a Jupyter notebook

--- PandasDataFrame.py
+++ PandasDataFrame.py
@@ -1,6 +1,6 @@
 #Objective: Dive deeper into Pandas DataFrame
 
-import pandas as pd
+import pandas as pd #Import needed to work with Pandas
 
 DataFrameData = pd.read_csv("https://media.geeksforgeeks.org/wp-content/uploads/nba.csv") #Create Pandas DataFrame from a URL
 FirstTenRows = DataFrameData.head(10) #Stores first 10 rows
@@ -10,31 +10,29 @@ BottomTenRows = DataFrameData.tail(10) #Stores bottom 10 rows
 print(BottomTenRows,"\n\n")
 
 FirstFourColNames = list(DataFrameData)[:4] #Stores first 4 column names
-FirstFourCols = DataFrameData[FirstFourColNames] #Stores first 4 columns
+FirstFourCols = DataFrameData.iloc[:4,:4] #Stores first 4 rows and first four columns
+#Use .iloc[] / loc[] when possible. Using df[] can cause warning errors
 print(FirstFourCols,"\n\n")
 
 SummaryData = DataFrameData.describe(); #Stores a analytical DataFrame
 print(SummaryData,"\n\n")
 
-SubSetData = FirstFourCols.head(4); #Stores the first 4 rows and columns 
-print(SubSetData,"\n\n")
-
 States = ['CA','WA','MA','NC'] #Stores the values for the new column
-SubSetData["States"] = States #Creates a new column with the column values
-print("Add Column Example:","\n",SubSetData,"\n\n")
+FirstFourCols["States"] = States #Creates a new column with the column values
+print("Add Column Example:","\n",FirstFourCols,"\n\n")
 
 DeleteCols = ["Team", "Number"] #Stores column names to be deleted
-SubSetData.drop(DeleteCols,axis = 1, inplace = True) #Deletes to the columns
-print("Delete Column Example:","\n",SubSetData,"\n\n")
+FirstFourCols.drop(DeleteCols,axis = 1, inplace = True) #Deletes to the columns
+print("Delete Column Example:","\n",FirstFourCols,"\n\n")
 
 RowValues = {"Name":"William Dunston","Position":"WR","States":"NC"} #Stores the values for the new row
 NewRow = pd.DataFrame(RowValues,index=[0]) #Create a 1 row DataFrame
-SubSetData = pd.concat([NewRow,SubSetData]).reset_index(drop=True) #Concatenate SubSetData and NewRow
-print("Add Row Example:","\n",SubSetData,"\n\n")
+FirstFourCols = pd.concat([NewRow,FirstFourCols]).reset_index(drop=True) #Concatenate FirstFourCols and NewRow
+print("Add Row Example:","\n",FirstFourCols,"\n\n")
 
 DeleteRows = [2] #Stores the row index(es) to be deleted
-SubSetData.drop(DeleteRows,inplace = True)
-print("Delete Row Example:","\n",SubSetData,"\n\n") #Deletes the row(s) from the DataFrame
+FirstFourCols.drop(DeleteRows,inplace = True)
+print("Delete Row Example:","\n",FirstFourCols,"\n\n") #Deletes the row(s) from the DataFrame
 
 TruncateDataFrameData = DataFrameData.truncate(before = 5, after = 9) #Only displays rows after 5th row and before 9th row
 TruncateColumns = list(TruncateDataFrameData) #Store all columns
@@ -50,7 +48,11 @@ print("Summed Up Group Values \n",GroupTeamsSum,"\n\n")
 
 GroupTeamsAndNumber = DataFrameData.groupby(["Team","Number"]) #Group DataFrame by Teams and Numbers
 
-print("Group Loop Example:")
+print("Group Loop Example (3x3 for space): ")
 for teams, group in GroupTeams: #Loop through all the team groups
-    print(teams) #Loop teams
-    print(group) #Loop group values
+    print(teams) #Loop teams (string)
+    print(group.iloc[:3,:3],"\n") #Loop group values (dataframe)
+    
+print("Group Select Example (3x3 for space): ")
+BrooklynNets = GroupTeams.get_group("Brooklyn Nets").iloc[:3,:3] #Grabs the first 3 entries in the Brooklyn Nets group
+print(BrooklynNets)
